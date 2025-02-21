36aae4184c12c91ae3c5bc06c6775c9dc73f40b9
Milan Belal <mohitbelal9@gmail.com>
2025-02-21 13:54:22 +0545

    day 2 all tasks completed

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('helloMilan');
+});
 Route::get('/home', function () {
     return view('home');
 })->name('Homepage');
@@ -19,3 +19,13 @@ Route::get('/about/{name?}', function ($name="") {
     return view('about',['name'=>$name]);
 });
 //Route::redirect('/','/home');
+//multiple parameters
+Route::get('/users/{postId}/comment/{commentId}', function ($postId, $commentId) {
+    return "Post ID: $postId, Comment ID: $commentId";
+});
+Route::get('milan/{name}/{id}',function($name,$id){
+return "name is: .$name age is .$id ";
+});
+route::get('first/{name}/{id}' ,function($name,$id){
+return view('first',['name'=>$name,'id'=>$id]);
+});
