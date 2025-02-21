d0e4c3498d883eccde909fd66f2839d2742c207a
Milan Belal <mohitbelal9@gmail.com>
2025-02-21 14:55:36 +0545

    learned sub routes using prefix

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,29 @@ Route::get('/about/{name}', function ($name) {
     return view('about');
 });
 //optional parameter 
-Route::get('/about/{name?}', function ($name="") {
-   
-    return view('about',['name'=>$name]);
+Route::get('/about/{name?}', function ($name = "") {
+
+    return view('about', ['name' => $name]);
 });
 //Route::redirect('/','/home');
 //multiple parameters
 Route::get('/users/{postId}/comment/{commentId}', function ($postId, $commentId) {
     return "Post ID: $postId, Comment ID: $commentId";
 });
-Route::get('milan/{name}/{id}',function($name,$id){
-return "name is: .$name age is .$id ";
+Route::get('milan/{name}/{id}', function ($name, $id) {
+    return "name is: .$name age is .$id ";
 });
-route::get('first/{name}/{id}' ,function($name,$id){
-return view('first',['name'=>$name,'id'=>$id]);
+route::get('first/{name}/{id}', function ($name, $id) {
+    return view('first', ['name' => $name, 'id' => $id]);
+});
+
+//sub routes
+route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "Admin Dashboard";
+
+    });
+    Route::get('/users',function(){
+        return "manage users";
+    });
 });
