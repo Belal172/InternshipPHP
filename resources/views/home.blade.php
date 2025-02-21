e17762ff504aa234801e911e61ccbf54a63d3ca3
Milan Belal <mohitbelal9@gmail.com>
2025-02-21 11:49:00 +0545

    routes,named routes redirect completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h1>hello Aman dai .</h1><br>
+<h2>this is Molan Belal</h2>
+<a href="/about/milan">About page</a>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('helloMilan');
+Route::get('/home', function () {
+    return view('home');
+})->name('Homepage');
+Route::get('/about/{name}', function ($name) {
+    echo $name;
+    return view('about');
 });
+//optional parameter 
+Route::get('/about/{name?}', function ($name="") {
+   
+    return view('about',['name'=>$name]);
+});
+//Route::redirect('/','/home');
